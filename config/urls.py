ef3f7970980c045bb45347470896a36bcf3226a6
Michael Khaimov <michael.khaimov@raccoongang.com>
2017-12-30 23:16:40 +0200

    Update:
    * Qset views update: more modularity to templates and general qsets list view update
    * Redirect from the index page to the user organizations (till the index page will be ready)
    * Fix init-dev-env.sh script

--- config/urls.py
+++ config/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    url(r'^$', RedirectView.as_view(url='askup/organizations', permanent=False), name='index'),
     url(r'^admin/', admin.site.urls),
     url(r'^login/$', auth_views.login, name='login'),
     url(r'^logout/$', auth_views.logout, name='logout'),
