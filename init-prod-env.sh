fbc01539f0ffc60fc9f1dbe1cd1eb7bf275208f9
Michael Khaimov <michael.khaimov@raccoongang.com>
2018-01-17 12:03:39 +0200

    Removed an invalid static for the admin panel.

--- init-prod-env.sh
+++ init-prod-env.sh
@@ -4,4 +4,5 @@
 touch /.initiated
 python manage.py migrate
 python manage.py loaddata askup/fixtures/groups.json
+python manage.py collectstatic --noinput
 [[ "$MOCKUPDATA" ]] && python manage.py loaddata askup/fixtures/mockup_data.json
