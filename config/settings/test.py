2b4572cc5ad70955dcbcf3140d51348ea5ad383a
Michael Khaimov <michael.khaimov@raccoongang.com>
2018-02-21 18:51:30 +0200

    Test configuration now compatible with local testing

--- config/settings/test.py
+++ config/settings/test.py
@@ -2,7 +2,12 @@ from .dev import *  # noqa: F401,F403
 
 DATABASES['default']['USER'] = 'test'  # noqa: F405
 DATABASES['default']['NAME'] = 'travis_test_db'  # noqa: F405
-DATABASES['default']['HOST'] = 'localhost'  # noqa: F405
+
+try:
+    import config.settings.secure as secure
+except ImportError:
+    import config.settings.secure_example as secure
+    DATABASES['default']['HOST'] = 'localhost'  # noqa: F405
 
 SECRET_KEY = 'KEY'
 
--- test.sh
+++ test.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-sed 's#localhost#postgres#g' -i config/settings/test.py
 docker exec -it askupdev_django_1 python /askup/manage.py test
-sed 's#postgres#localhost#g' -i config/settings/test.py
