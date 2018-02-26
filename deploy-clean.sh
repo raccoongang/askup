5fa8f3b79162ed2e70eae9d7daa1f5fdb50214c1
Michael Khaimov <michael.khaimov@raccoongang.com>
2018-02-26 11:51:22 +0200

    Sudo suggestion update. readlink changed to realpath

--- deploy-clean.sh
+++ deploy-clean.sh
@@ -6,10 +6,11 @@ else
     DCENV="$1"
 fi
 
-cd `dirname $(readlink -f $0)`/.dc-$DCENV
+COMPOSE_DIR=`dirname $(realpath $0)`/.dc-$DCENV
+cd $COMPOSE_DIR
 
 [[ $? != 0 ]] && echo "Such environment isn't defined" && exit 1
 
 docker-compose down
-sudo rm -rf .volumes
-sudo docker-compose build && sudo docker-compose up -d
+sudo rm -rf $COMPOSE_DIR/.volumes
+sudo docker-compose build && docker-compose up -d
