6f4d28f9b0cdc4e28894dfc833a9e7be3937890e
Michael Khaimov <khaimovmr@gmail.com>
2018-03-30 12:26:26 +0300

    Docker containers fix. (#45)

--- deploy-clean.sh
+++ deploy-clean.sh
@@ -6,11 +6,18 @@ else
     DCENV="$1"
 fi
 
-COMPOSE_DIR=`dirname $(realpath $0)`/.dc-$DCENV
+REPO_DIR=`dirname $(realpath $0)`
+COMPOSE_DIR=$REPO_DIR/.dc-$DCENV
 cd $COMPOSE_DIR
 
 [[ $? != 0 ]] && echo "Such environment isn't defined" && exit 1
 
 docker-compose down
 sudo rm -rf $COMPOSE_DIR/.volumes
+sudo rm -rf $REPO_DIR/.initialized
 sudo docker-compose build && docker-compose up -d
+
+if [[ "$DCENV" == "dev" ]]
+then
+    docker-compose logs -f
+fi
--- start-server.sh
+++ start-server.sh
@@ -3,12 +3,14 @@ if [[ "$ASKUPENV" =~ (dev|test) ]]
 then
     while true
     do
+        echo "Running the runserver for a dev purposes..."
         python manage.py runserver 0.0.0.0:8001
         sleep 5
     done
 else
     while true
     do
+        echo "Running the gunicorn application server..."
         gunicorn config.wsgi:application -w 2 -b :8001 --log-level=info
         sleep 5
     done
