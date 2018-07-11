06b9964dc7395ed1b32e223995bfa293fad7324e
Michael Khaimov <michael.khaimov@raccoongang.com>
2018-07-11 14:17:05 +0300

    PR updates.

--- config/celery.py
+++ config/celery.py
@@ -3,6 +3,3 @@ from celery import Celery
 app = Celery('config')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
-
-if __name__ == '__main__':
-    app.start()
